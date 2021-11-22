3f42b115b3928533fe684e1f21f1449d85a0ee72
Paz-Razabi <pazrazabi@gmail.com>
2021-11-22 13:05:58 +0200

    Added the logo for the main activity.

--- app/src/main/java/com/paz/razabi/driving/MainActivity.java
+++ app/src/main/java/com/paz/razabi/driving/MainActivity.java
@@ -3,6 +3,7 @@ package com.paz.razabi.driving;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
