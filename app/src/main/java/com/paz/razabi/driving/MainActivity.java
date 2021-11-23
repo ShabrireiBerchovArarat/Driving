8b3078ba862fe0819b87922e8c415efa51e3de9c
Paz-Razabi <pazrazabi@gmail.com>
2021-11-23 13:25:41 +0200

    Added the login activity

--- app/src/main/java/com/paz/razabi/driving/MainActivity.java
+++ app/src/main/java/com/paz/razabi/driving/MainActivity.java
@@ -2,8 +2,11 @@ package com.paz.razabi.driving;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast timerToast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+        new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long l) {
+                timerToast.cancel();
+                timerToast.setText("Wait... ");
+                timerToast.show();
+            }
+
+            @Override
+            public void onFinish() {
+                Intent go = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(go);
+            }
+        }.start();
     }
 }
