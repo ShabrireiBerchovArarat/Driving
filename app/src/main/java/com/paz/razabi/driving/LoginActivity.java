39f9f3351194e894c3fd6e27f7dbb17dc11cbfc3
Paz-Razabi <pazrazabi@gmail.com>
2021-12-12 13:35:13 +0200

    Improved the login activity

--- app/src/main/java/com/paz/razabi/driving/LoginActivity.java
+++ app/src/main/java/com/paz/razabi/driving/LoginActivity.java
@@ -2,10 +2,18 @@ package com.paz.razabi.driving;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private SharedPreferences mPrefrences;
+    private SharedPreferences.Editor mEditor;
+    private EditText etUsername , etPassword;
+    private Button bSignIn , bSignUp;
+    private CheckBox cnRemember;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
