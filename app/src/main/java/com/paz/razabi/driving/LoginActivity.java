b6699b611c0b68eeff6375e319cfcd2d6b9f5e2d
Paz-Razabi <pazrazabi@gmail.com>
2021-12-13 13:40:07 +0200

    Added SharedPreferences support. (in LoginActivity).

--- app/src/main/java/com/paz/razabi/driving/LoginActivity.java
+++ app/src/main/java/com/paz/razabi/driving/LoginActivity.java
@@ -4,19 +4,82 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private SharedPreferences mPrefrences;
+    private SharedPreferences mPreferences;
     private SharedPreferences.Editor mEditor;
-    private EditText etUsername , etPassword;
+    private EditText etEmail , etPassword;
     private Button bSignIn , bSignUp;
-    private CheckBox cnRemember;
+    private CheckBox cbRemember;
+    private final String EMPTY_OR_MISSING_SP = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        bSignIn = findViewById(R.id.bSignIn);
+        bSignUp = findViewById(R.id.bSignUp);
+
+        cbRemember = findViewById(R.id.cbRemember);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+        // If possible, restore saved credentials.
+        restoreCreds();
+
+        // Else, continue with sign in/up
+
+    }
+
+    private void restoreCreds() {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        cbRemember = findViewById(R.id.cbRemember);
+
+        etEmail.setText(mPreferences.getString("saved_email", EMPTY_OR_MISSING_SP));
+        etPassword.setText(mPreferences.getString("saved_password", EMPTY_OR_MISSING_SP));
+        cbRemember.setChecked(mPreferences.getBoolean("saved_creds", false));
+    }
+
+    public void signUp(View view) {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        cbRemember = findViewById(R.id.cbRemember);
+
+        if (cbRemember.isChecked()) {
+            mEditor.putString("saved_email", etEmail.getText().toString());
+            mEditor.putString("saved_password", etPassword.getText().toString());
+            mEditor.putBoolean("save_creds", true);
+        } else {
+            mEditor.putString("saved_email", EMPTY_OR_MISSING_SP);
+            mEditor.putString("saved_password", EMPTY_OR_MISSING_SP);
+            mEditor.putBoolean("save_creds", false);
+        }
+        mEditor.apply();
+    }
+
+    public void signIn(View view) {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        if (cbRemember.isChecked()) {
+            mEditor.putString("saved_email", etEmail.getText().toString());
+            mEditor.putString("saved_password", etPassword.getText().toString());
+            mEditor.putBoolean("save_creds", true);
+        } else {
+            mEditor.putString("saved_email", EMPTY_OR_MISSING_SP);
+            mEditor.putString("saved_password", EMPTY_OR_MISSING_SP);
+            mEditor.putBoolean("save_creds", false);
+        }
+        mEditor.apply();
     }
 }
